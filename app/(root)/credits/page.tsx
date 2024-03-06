2c961639d34577f3b3c260bd914b1db87983703b
Venkata Rao Dasari <venkat@venkatas-imac.lan>
2024-03-05 19:07:57 -0600

    initial commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const creditsPage = () => {
+  return (
+    <div>creditsPage</div>
+  )
+}
+
+export default creditsPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const updateTransformationsPage = () => {
+  return (
+    <div>updateTransformationsPage</div>
+  )
+}
+
+export default updateTransformationsPage
